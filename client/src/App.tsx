d4bc5a7ba4f30b75cda3a49df6f4b65e34b69620
Cody Seibert <cseibert113@gmail.com>
2022-11-12 13:01:55 -0500

    working on showing the decks on the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+type TDeck = {
+  title: string;
+  _id: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [decks, setDecks] = useState<TDeck[]>([]);
+  const [title, setTitle] = useState("");
+
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault();
+    await fetch("http://localhost:5000/decks", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setTitle("");
+  }
+
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch("http://localhost:5000/decks");
+      const newDecks = await response.json();
+      setDecks(newDecks);
+    }
+    fetchDecks();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul className="decks">
+        {decks.map((deck) => (
+          <li key={deck._id}>{deck.title}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Create Deck</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,24 @@ config();
 
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import Deck from "./models/Deck";
 
 const PORT = 5000;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
+});
 
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
