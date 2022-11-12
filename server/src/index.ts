694bf528d0d53f8d2ff50b2d42cedbc85300d344
Cody Seibert <cseibert113@gmail.com>
2022-11-12 12:00:50 -0500

    working on setting up the ui and api, plus adding /decks endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import Deck from "./models/Deck";
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://flashcardsage:ig580lqrHBpmhmCG@cluster0.74strjf.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`listening on port ${PORT}`);
+    app.listen(PORT);
+  });
---
+++ server/src/models/Deck.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+// const ObjectId = mongoose.Types.ObjectId;
+
+const DeckSchema = new Schema({
+  title: String,
+});
+
+const DeckModel = mongoose.model("Deck", DeckSchema);
+
+export default DeckModel;
