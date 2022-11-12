3b9504f24f980ede74616355ddcf040c046e15c0
Cody Seibert <cseibert113@gmail.com>
2022-11-12 12:07:34 -0500

    adding dotenv to secure the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
+import { config } from "dotenv";
+config();
+
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-
 import Deck from "./models/Deck";
 
 const PORT = 5000;
@@ -17,11 +19,7 @@ app.post("/decks", async (req: Request, res: Response) => {
   res.json(createdDeck);
 });
 
-mongoose
-  .connect(
-    "mongodb+srv://flashcardsage:ig580lqrHBpmhmCG@cluster0.74strjf.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log(`listening on port ${PORT}`);
-    app.listen(PORT);
-  });
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
+});
